f7648ba0746e67f90fafd3ceba6f66d3048bfee9
Ayah Elshawa <ayahs19302@gmail.com>
2023-05-23 02:47:20 +0300

    almost finished advisor component

--- blueprints/advisor_blueprint.py
+++ blueprints/advisor_blueprint.py
@@ -1,7 +1,42 @@
-from flask import Flask, jsonify, render_template, Blueprint
+from flask import Flask, jsonify, render_template, Blueprint, request
 from controller import advisor_controller
 advisor_blueprint = Blueprint("advisor", __name__)
 
 @advisor_blueprint.route('/advisor', methods=["GET"])
 def dashboard_view():
     return advisor_controller.index("token")
+
+
+@advisor_blueprint.route('/advisor/trainees/pending', methods=["GET"])
+def pending_trainees():
+    return advisor_controller.get_pending_trainees(request)
+
+
+@advisor_blueprint.route('/advisor/traineesatte/active', methods=["GET"])
+def active_trainees():
+    return advisor_controller.get_active_trainees(request)
+
+
+@advisor_blueprint.route('/program', methods=["GET"])
+def program_material():
+    return advisor_controller.get_program_materials(request)
+
+
+@advisor_blueprint.route('/advisor/meetings', methods=["GET"])
+def meetings():
+    return advisor_controller.get_meeting_requests(request)
+
+
+@advisor_blueprint.route('/advisor/reschedule', methods=["GET"])
+def reschedule():
+    return advisor_controller.get_meeting_form(request)
+
+
+@advisor_blueprint.route('/advisor/meeting/create', methods=["GET"])
+def create_meeting():
+    return advisor_controller.get_meeting_form(request)
+
+
+@advisor_blueprint.route('/advisor/trainee/attendance', methods=["GET"])
+def attendance_form():
+    return advisor_controller.get_attendance_form(request)
--- controller/advisor_controller.py
+++ controller/advisor_controller.py
@@ -1,13 +1,71 @@
 from flask import render_template
 
+advisor = {
+    "id": "20",
+    "username": "venus2023",
+    "email": "someemail@gmail.com",
+    "discipline": "Python Programming",
+    "department": "comp sci",
+    "interest": "AI",
+    "phone": "123456",
+    "max_number_of_trainees": 5,
+    "img":"../../static/assets/img/profile-img.jpg"
+}
+
+programs = [
+    ["1", "program1", "program1 description", "area1", "money", "somedate", "somedate"],
+    ["2", "program2", "program2 description", "area1", "money", "somedate", "somedate"],
+    ["3", "program3", "program3 description", "area1", "money", "somedate", "somedate"],
+]
+
+trainees = [
+    ["1", "name1", "email1", "df1", "area1"],
+    ["2", "name2", "email2", "df1", "area1"],
+    ["3", "name3", "email3", "df1", "area1"]
+]
+
+meetings = [
+    ["meetingid1", "meeting for followup", "Approved", "https://linktoyou",
+     "2001-09-09", "15:15", "14:14", "trainee id", "advisor id"],
+    ["meetingid2", "meeting for followup", "Conflict", "https://linktoyou",
+     "2001-09-09", "15:15", "14:14", "trainee id", "advisor id"],
+    ["meetingid3", "meeting for followup", "Approved", "https://linktoyou",
+     "2001-09-09", "15:15", "14:14", "trainee id", "advisor id"]
+]
+
+
 
 def index(token):
     # from token, fetch advisor id, then fetch the record from the database
-    advisor = {
-        "id": "20",
-        "username": "venus2023",
-        "email": "someemail@gmail.com",
-        "discipline": "Python Programming",
-        "max_number_of_trainees": 5,
-    }
-    return render_template('advisor/index.html', advisor=advisor)
+    
+    return render_template('advisor/index.html', advisor = advisor)
+
+
+def get_pending_trainees(request):
+    return render_template('advisor/pending_trainees.html', trainees = trainees, advisor = advisor)
+
+
+def get_active_trainees(request):
+    return render_template('advisor/my_trainees.html', trainees = trainees, advisor = advisor)
+
+
+def get_program_materials(request):
+    return render_template('advisor/my_programs.html', programs = programs, advisor = advisor)
+
+
+def get_meeting_requests(request):
+    return render_template('advisor/meetings.html', meetings = meetings, advisor = advisor)
+
+
+def get_reschedule_request(request):
+    return render_template('advisor/reschedule.html', meeting = meetings[0], advisor = advisor)
+
+
+def get_meeting_form(request):
+    return render_template('advisor/create_meeting.html', advisor = advisor)
+
+
+def get_attendance_form(request):
+    return render_template('advisor/attendance_form.html', advisor = advisor, trainee = trainees[0])
+
+
