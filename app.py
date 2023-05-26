204b1d69f4968e4ebc9348a468b6108eeaa23606
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2023-05-27 02:38:15 +0300

    for deployment purposes, i've deleted main.py and made app.py as the runner file

--- app.py
+++ app.py
@@ -34,3 +34,7 @@ app.register_blueprint(advisor_blueprint)
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000, debug=True)
+
